a9e54fd8bebd4244ded94ecab379848a2ca2a7ca
tuan0505 <tuan557552@gmail.com>
2025-06-20 16:28:42 +0700

    update main from ubuntu

--- src/main/java/com/nvt/web/Application.java
+++ src/main/java/com/nvt/web/Application.java
@@ -3,7 +3,7 @@ package com.nvt.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Branch main
+//Branch main ubuntu
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
