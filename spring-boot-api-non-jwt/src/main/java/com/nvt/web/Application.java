8afe27eb40793042d3df7d6b139a0b8da7e5076a
tuan0505 <tuan557552@gmail.com>
2025-06-20 16:50:18 +0700

    update JDBC from ubuntu

--- spring-boot-api-non-jwt/src/main/java/com/nvt/web/Application.java
+++ spring-boot-api-non-jwt/src/main/java/com/nvt/web/Application.java
@@ -3,6 +3,7 @@ package com.nvt.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//ubuntu  JDBC
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
